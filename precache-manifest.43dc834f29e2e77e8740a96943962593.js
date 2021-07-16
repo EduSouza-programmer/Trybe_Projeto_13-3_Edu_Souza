21e8bfeaca28e82fa48b6dcc01007ee5765c64db
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-16 13:09:41 -0300

    Updates

---
+++ precache-manifest.43dc834f29e2e77e8740a96943962593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6473c03cc475446d9591a73f0262f0",
+    "url": "/Trybe_Projeto_13-3_Edu_Souza/index.html"
+  },
+  {
+    "revision": "9e3432fd6fadc7457f22",
+    "url": "/Trybe_Projeto_13-3_Edu_Souza/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1643d7ba5d04fadaddfb",
+    "url": "/Trybe_Projeto_13-3_Edu_Souza/static/js/2.7ba545bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trybe_Projeto_13-3_Edu_Souza/static/js/2.7ba545bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3432fd6fadc7457f22",
+    "url": "/Trybe_Projeto_13-3_Edu_Souza/static/js/main.8181d574.chunk.js"
+  },
+  {
+    "revision": "6d1ec105a8bb4ded0680",
+    "url": "/Trybe_Projeto_13-3_Edu_Souza/static/js/runtime-main.3a8b7ec4.js"
+  }
+]);
